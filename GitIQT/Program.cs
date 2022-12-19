e9f5fe9cf62d9a629b0adfcc07cca333623c0606
Yuri Sim <edaraks@gmail.com>
2022-12-18 22:45:14 -0600

    gram fix

--- GitIQT/Program.cs
+++ GitIQT/Program.cs
@@ -15,7 +15,7 @@
                 "3. Create a new branch called 'feature/feature-name' from the dev branch",
                 "4. Make some changes to the code",
                 "5. Commit the changes to the feature branch",
-                "6. Push the feature branch to the remote repository /r/n",
+                "6. Push the feature branch to the remote repository",
                 "======================================================================================================================",
                 "",
                 "======================",
